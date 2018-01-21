2f6b89b26270c174e387e27ce5038ff81ab32764
KameriiJ <jenny.sirarat@gmail.com>
2018-01-21 12:51:29 +0700

    add java doc

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -3,12 +3,27 @@ package coinpurse;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Test the Coin.
+ * MoneyUtil adds new coins that have different value or currency into the list,
+ * and orders value of coins in the list.(smaller to bigger)  
+ * 
+ * @author  Gunthee Tawewatmongkol
+ */
 public class MoneyUtil {
 	
+	/**
+	 * Order value of coins in the list.(smaller to bigger)
+	 * @param coins
+	 */
 	public static void sortCoins(List<Coin> coins) {
 		java.util.Collections.sort(coins);
 	}
 	
+	/**
+	 * Print all coins in the list.
+	 * @param coins
+	 */
 	public static void printCoins(List<Coin> coins) {
 		for(int x = 0;x < coins.size();x++) {
 			System.out.println(coins.get(x));
