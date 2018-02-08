1b30b730a6f8bbf6c86654e93066a42f6bc29c4b
KameriiJ <jenny.sirarat@gmail.com>
2018-02-08 16:03:09 +0700

    add alphabetical order currency in compare method

--- src/coinpurse/ValueComparator.java
+++ src/coinpurse/ValueComparator.java
@@ -7,10 +7,24 @@ public class ValueComparator implements Comparator<Valuable> {
 	/**
      * Compare two objects that implement Valuable.
      * If both objects have the same currency, order them by value.
+     * If the currencies are different, order them by alphabetical ordering.
      */
 	@Override
 	public int compare(Valuable a, Valuable b) {
-		if(a.getCurrency().equals(b.getCurrency())){
+		if (!a.getCurrency().equals(b.getCurrency())){
+			String currentA = a.getCurrency().toLowerCase();
+			String currentB = b.getCurrency().toLowerCase();
+			int length = Math.min(currentA.length(), currentB.length());
+			
+			for (int x = 0; x < length; x++) {
+				int chaA = currentA.charAt(x);
+				int chaB = currentB.charAt(x);
+				
+				if (chaA < chaB) return -1;
+				else if (chaA > chaB) return 1;
+			}
+		}
+		else {
 			if (a.getValue() > b.getValue()) return 1;
 			else if (a.getValue() < b.getValue()) return -1;
 		}
