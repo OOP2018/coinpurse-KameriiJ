bd4e0a20a06bdc4d71ab2f9c8aa68b4807669f3d
KameriiJ <jenny.sirarat@gmail.com>
2018-02-11 12:53:33 +0700

    fixed code

--- src/coinpurse/ValueComparator.java
+++ src/coinpurse/ValueComparator.java
@@ -11,27 +11,16 @@ public class ValueComparator implements Comparator<Valuable> {
      */
 	@Override
 	public int compare(Valuable a, Valuable b) {
-		if (!a.getCurrency().equals(b.getCurrency())){
-			String currentA = a.getCurrency().toLowerCase();
-			String currentB = b.getCurrency().toLowerCase();
-			int length = Math.min(currentA.length(), currentB.length());
-			int chaA = 0,chaB = 0;
-			for (int x = 0; x < length; x++) {
-				chaA = currentA.charAt(x);
-				chaB = currentB.charAt(x);
-				if (chaA < chaB) return -1;
-				else if (chaA > chaB) return 1;
-			}
-			if(chaA == chaB && currentA.length() != currentB.length()) {
-				if (currentA.length() < currentB.length()) return -1;
-				else return 1;
-			}
+		String currentA = a.getCurrency().toLowerCase();
+		String currentB = b.getCurrency().toLowerCase();
+		if(!currentA.equals(currentB)) {
+			if(currentA.compareTo(currentB) < 0) return -1;
+			else if(currentA.compareTo(currentB) > 0) return 1;
+			else return 0;
 		}
 		else {
-			if (a.getValue() > b.getValue()) return 1;
-			else if (a.getValue() < b.getValue()) return -1;
+			return Double.compare(a.getValue(), b.getValue());
 		}
-		return 0;
 	}
 	
 }
