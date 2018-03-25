30be2cb6e318349ce4aadafecdc89e4e6eef96b9
KameriiJ <jenny.sirarat@gmail.com>
2018-03-25 14:33:04 +0700

    fixed code

--- src/coinpurse/strategy/RecursiveWithdraw.java
+++ src/coinpurse/strategy/RecursiveWithdraw.java
@@ -1,42 +1,42 @@
 package coinpurse.strategy;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
+import coinpurse.Money;
 import coinpurse.Valuable;
 import coinpurse.ValueComparator;
 
 public class RecursiveWithdraw implements WithdrawStrategy{
 	
-	public static List<Valuable> helper(List<Valuable> list, List<Valuable> templist, double target, String currency) {
-		Valuable check;
-		try { check = list.get(0); } catch (Exception ex) { return null; }
+	@Override
+	public List<Valuable> withdraw(Valuable amount, List<Valuable> money) {
+		List<Valuable> templist = new ArrayList<Valuable>(money.size());
+		Comparator<Valuable> comp = new ValueComparator();
+		Collections.sort(money, comp);
+		Collections.reverse(money);
+		
+		return helper(money, templist, amount);
+	}
+	
+	public static List<Valuable> helper(List<Valuable> list, List<Valuable> templist, Valuable amount) {
+		Valuable check; double target; String currency;
+		try { 
+			check = list.get(0);
+			target = amount.getValue();
+			currency = amount.getCurrency();
+		} catch (Exception ex) {
+			return null; 
+		}
 		double result = target;
 		if(check.getCurrency().equalsIgnoreCase(currency)) {
-			result = target - check.getValue();
+			result -= check.getValue();
 			if(result >= 0) {
 				templist.add(check);
 			}
 			if(result < 0) result = target;
 		}
-		if (list.size() > 1) helper(list.subList(1, list.size()), templist, result, currency);
-		if (list.size() <= 1 && result != 0) return null;
-		return templist;
+		if (list.size() > 1) helper(list.subList(1, list.size()), templist, new Money(result, currency));
+		if (list.size() < 2 && result != 0) templist.clear();
+		return (templist.size() != 0) ? templist : null;
 	}
-
-	@Override
-	public List<Valuable> withdraw(Valuable amount, List<Valuable> money) {
-		List<Valuable> templist = new ArrayList<Valuable>(money.size());
-		if(amount == null || amount.getValue() == 0) return templist;
-		
-		Comparator<Valuable> comp = new ValueComparator();
-		java.util.Collections.sort(money, comp);
-		java.util.Collections.reverse(money);
-		
-		double target = amount.getValue();
-		
-		return helper(money, templist, target, amount.getCurrency());
-	}
-	
 }
